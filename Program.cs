414f44a784af1398cadfa52bafdec87edddf0c19
Tahil40 <116889476+Tahil40@users.noreply.github.com>
2022-12-06 23:31:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System; 
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // Creating a Calculator....
+            Console.WriteLine("Enter The Firt Number; ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter The Second Number; ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Chosee from Given MeThamatical Operators(+) (-) (/) (*)");
+            String num3 = Console.ReadLine();
+
+            if (num3 == "+")
+            {
+                int add = (num1 + num2);
+                Console.WriteLine("The Sum of The two Numbers is " + add);
+            }
+
+            else if (num3 == "-")
+            {
+                int minus = (num1 - num2);
+                Console.WriteLine("The Subtraction of Two Numbers is " + minus);
+            }
+
+            else if (num3 == "*")
+            {
+                int mul = (num1 * num2);
+                Console.WriteLine("The Multiplication of Two Numbers is " + mul);
+            }
+
+            else if (num3 == "/")
+            {
+                int div = (num1 / num2);
+                Console.WriteLine("The Division of Two Numbers is " + div);
+            }
+
+            else
+            {
+                Console.WriteLine("Something wents Wrong, Please Check your Input and Try again....");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
